dfcca616ab6c4c9955b3b537b960843ce9c1d67d
Jakub Spórna <jakub.sporna@gmail.com>
2023-11-19 15:04:32 +0100

    updated styles of vue components

--- src/.vitepress/theme/components/PostPreview.vue
+++ src/.vitepress/theme/components/PostPreview.vue
@@ -1,12 +1,13 @@
 <template>
-  <a :href="href" class="flex flex-shrink-0 xl:flex-grow-0 w-full xl:w-[38rem] p-3 border-2 rounded-2xl hover:border-sky-500">
-    <div class="">
-      <div class="flex w-full">
-        <div class="flex flex-col ml-4">
-          <h3 class="w-full text-xl font-bold">{{ title }}</h3>
-          <small>{{ date }}</small>
-          <p>{{ excerpt }}</p>
-        </div>
+  <a :href="href">
+    <div class="post">
+      <div class="post-img">
+        <img :src="image" :alt="title"/>
+      </div>
+      <div class="post-info">
+        <h3>{{ title }}</h3>
+        <small>{{ date }}</small>
+        <p>{{ excerpt }}</p>
       </div>
     </div>
   </a>
@@ -31,6 +32,57 @@ export default {
       type: String,
       required: true,
     },
+    image: {
+      type: String,
+      required: false
+    }
   },
 };
 </script>
+
+<style>
+.post {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 0px;
+  margin-bottom: 10px;
+  padding: 10px 10px;
+  border: 1px solid grey;
+}
+
+.post-img img{
+  width: 450px;
+  height: 300px;
+  float: left
+}
+
+.post-info {
+  padding-top: 20px;
+  padding-left: 20px;
+}
+
+.post-info h3 {
+  margin-top: 5px;
+  height: 33px;
+  font-size: 24px;
+  font-weight: 600;
+}
+
+.post-info small {
+  color: red;
+  font-weight: 300;
+}
+
+.post-info p {
+  width: 70%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    grid-template-columns: 1fr 2fr;
+  }
+  .post-img img {
+    float: right;
+  }
+}
+</style>
--- src/.vitepress/theme/components/Posts.vue
+++ src/.vitepress/theme/components/Posts.vue
@@ -4,7 +4,14 @@ import { data as articles } from '../blog.data'
 </script>
 
 <template>
-  <div class="container mx-auto flex flex-wrap justify-center gap-x-4 gap-y-4">
-    <BlogPostPreview v-for="article in articles" :title="article.title" :href="article.url" :date="article.date.string" :excerpt="article.excerpt"/>
+  <div class="posts">
+    <BlogPostPreview v-for="article in articles" :title="article.title" :href="article.url" :date="article.date.string" :excerpt="article.excerpt" :image="article.image"/>
   </div>
 </template>
+
+<style>
+.posts {
+  margin: 20px auto 0;
+  max-width: calc(var(--vp-layout-max-width) - 64px);
+}
+</style>
